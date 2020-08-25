77c3a127b98ca3a397c5282ecc580457213835d5
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-08-25 19:44:03 -0400

    Updates

---
+++ precache-manifest.5b4a69c9cc610db94a55e29682197e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90e7afcad79786fa76b73f3b2a09148",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "dc0880f5b09a03bc59d4",
+    "url": "/weather-app-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "025bd730f3842c3fda8b",
+    "url": "/weather-app-react/static/js/2.c2cb6ebb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react/static/js/2.c2cb6ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0880f5b09a03bc59d4",
+    "url": "/weather-app-react/static/js/main.b31f9e23.chunk.js"
+  },
+  {
+    "revision": "4b38b70320efdd3fccf7",
+    "url": "/weather-app-react/static/js/runtime-main.e86ff5fe.js"
+  }
+]);
